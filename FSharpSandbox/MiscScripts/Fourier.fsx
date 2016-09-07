568b29d758f794051fe4ef1dcfae2236ada4aabb
OctoTom <tomas.janda@outlook.com>
2016-09-07 08:43:48 +0200

    Earthquake generation. The signal is modulated but the resulting spectrum is not compatible with the prescribed spectru. Amplitudes heed to be weighted.

---
+++ FSharpSandbox/MiscScripts/Fourier.fsx
@@ -0,0 +1,148 @@
+﻿#r @"c:\Users\Tomas\Sync\Timber\LepenyNosnik\packages\MathNet.Numerics.3.11.0\lib\net40\MathNet.Numerics.dll"
+#r @"c:\Users\Tomas\Sync\Timber\LepenyNosnik\packages\MathNet.Numerics.FSharp.3.11.0\lib\net40\MathNet.Numerics.FSharp.dll"
+#r @"c:\Users\Tomas\OneDrive\OneSync\Projects\CommonTools\CommonTools\Bin\Release\CommonTools.dll"
+#load @"c:\Users\Tomas\Scripts\Paket\LoadPackages.fsx"
+
+open System
+open System.Runtime.InteropServices
+open System.IO
+open System.Numerics // System.Numerics.Complex
+open MathNet.Numerics
+open MathNet.Numerics.LinearAlgebra
+open FSharp.Charting
+
+
+// Playground for Forward FFT and Inverse FFT transformations.
+// ===========================================================
+
+// Helper functions
+let c x = Complex(x, 0.0)
+let i = Complex.ImaginaryOne
+let exp = MathNet.Numerics.Complex.exp
+let rand = System.Random()
+module List =
+  let mapio mapping list = List.map (fun x -> (x, mapping x)) list
+
+// --------------------- //
+// Play around with FFT. //
+// --------------------- //
+module Fourier =
+  /// Spectrum
+  type spectrum = {freqBins : float list; spectralAmplitudes : float list}
+  /// History (time series)
+  type history = {times : float list; amplitudes : float list}
+  /// Scaling options of the FFT.
+  let options = MathNet.Numerics.IntegralTransforms.FourierOptions.Matlab
+  // Scaling of the Fourier amplitudes
+  // https://youtu.be/dM1y6ZfQkDU?t=991
+  let matlabAmpToRealAmp n matlabAmplitude = 2.0 * matlabAmplitude / float n
+  let realAmpToMatlabAmp n realAmplitude = float n * realAmplitude / 2.0
+  /// Wrapper of forward FFT
+  let fftf (list : Complex list) =
+    let res = (list |> List.toArray).Clone() :?> (Complex [])
+    MathNet.Numerics.IntegralTransforms.Fourier.Forward(res, options) 
+    res |> Array.toList
+  /// Wrapper of inverse FFT 
+  let ffti (list : Complex list) =
+    let res = (list |> List.toArray).Clone() :?> (Complex [])
+    MathNet.Numerics.IntegralTransforms.Fourier.Inverse(res, options) 
+    res |> Array.toList
+  /// Transforms real spectrum to real history.
+  /// Only the first half of spectrum is used.
+  let spectrumToHistory (spectrum : spectrum) =
+    let fsHalf = spectrum.freqBins |> List.rev |> List.head
+    let fs = 2.0 * fsHalf
+    let dt = 1.0 / fs // Time step
+    let nHalf = spectrum.freqBins |> List.length 
+    let n = 2 * nHalf - 1
+    let times = List.init n (fun i -> float i * dt)
+    let spectralAmplitudes = spectrum.spectralAmplitudes @ List.replicate (nHalf - 1) 0.0
+    let amplitudes = spectralAmplitudes |> List.map (fun x -> realAmpToMatlabAmp nHalf x |> c) |> ffti |> List.map (fun x -> x.Real)
+    {times = times; amplitudes = amplitudes}
+  /// Transforms real history to real spectrum
+  /// Only the first half of symmetric spectrum is returned
+  let historyToSpectrum (history : history) =
+    let dt = history.times |> List.tail |> List.head
+    let fs = 1.0 / dt
+    let n = history.times |> List.length
+    if n % 2 <> 1 then failwith "Number of data points in time history must be odd."
+    let nHalf = (n + 1) / 2
+    let freqBins = List.init n (fun i -> float i / float n * fs) |> List.take nHalf
+    let spectralAmplitudes = history.amplitudes |> List.map c |> fftf |> List.map (fun x -> x.Real |> matlabAmpToRealAmp nHalf) |> List.take nHalf
+    {freqBins = freqBins; spectralAmplitudes = spectralAmplitudes}
+
+/// Eurocode spectrum in time domain
+module Spectrum =
+  // For given parameters returns spectrum as a function of time period T [s].
+  let getSpectrum (tb, tc, td, eta, ag, s) =
+    let se t =
+      match t with
+      | t when t < 0.0 -> failwith "Negative period time T."
+      | t when t < tb -> ag * s * (1.0 + t / tb * (2.5 * eta - 1.0))
+      | t when t < tc -> 2.5 * eta * ag * s
+      | t when t < td -> 2.5 * eta * ag * s * tc / t
+      | t when t < 4.0 -> 2.5 * eta * ag * s * tc * td / t**2.0
+      | _ -> 0.0 // For periods longer than 4 seconds.
+    se
+
+open Fourier
+  
+/// Creates spectrum given number of samples n, highest frequency fs and function defining the spectrum.
+let createSpectrum nHalf fsHalf (spectrumFunc : float -> float) =
+  let freqBins = [0.0 .. fsHalf / float (nHalf - 1) .. fsHalf]
+  let spectralAmplitudes = freqBins |> List.map spectrumFunc
+  {freqBins = freqBins; spectralAmplitudes = spectralAmplitudes}
+/// Plots spectrum as line chart
+let drawSpectrum (spectrum : spectrum) =
+  List.zip spectrum.freqBins spectrum.spectralAmplitudes |> Chart.FastLine
+/// Plots history as line chart
+let drawHistory (history : history) =
+  List.zip history.times history.amplitudes |> Chart.FastLine
+
+
+// Get Eurocode spectrum in period domain
+let mySpectrumInT = Spectrum.getSpectrum (0.1, 0.5, 2.0, 0.5, 1.0, 1.0)
+// Eurocode spectrum in frequency domain
+let mySpectrum f =
+  match f with
+  | f when f < 0.1 -> 0.0
+  | f when f > 25.0 -> 0.0
+  | f -> mySpectrumInT (1.0 / f)
+
+
+// Sampling frequency
+let fs = 100.0
+// Number of points
+let n = 1001
+// Size of frequency bin
+let df = fs / float (n - 1)
+
+// List of random phases
+let phases =
+  let getRandomPhase () = rand.NextDouble() * 2.0 * System.Math.PI
+  List.init n (fun _ -> getRandomPhase ())
+
+// Bin's frequencies
+let fList = [0.0 .. df .. fs]
+// Bins (tuples with frequency and spectral amplitude).
+let spectrumAux = fList |> List.mapio mySpectrum
+let spectrum = List.map2 (fun (f, magnitude) phase -> f, Complex.CreatePolar(magnitude, phase)) spectrumAux phases
+// Signal obtained by forward FFT. Phases are random.
+let signal = spectrum |> List.map snd |> ffti |> List.map (fun z -> z.Real)
+// Output spectrum
+let outputSpectrum = signal |> List.map c |> fftf |> List.map (fun z -> z.Magnitude) 
+let signalModulated =
+  let mapping i x =
+    match i with
+    | i when i < n / 2 -> float i / float n * 2.0 * x
+    | i when i < n -> (2.0 - float i / float n * 2.0) * x
+    | _ -> 0.0
+  signal |> List.mapi mapping
+let outputSpectrumModulated = signalModulated |> List.map c |> fftf |> List.map (fun z -> z.Magnitude) 
+
+spectrum |> List.map (fun (f, z) -> f, z.Magnitude) |> Chart.FastLine
+signal |> Chart.FastLine
+outputSpectrum |> Chart.FastLine
+signalModulated |> Chart.FastLine
+outputSpectrumModulated |> Chart.FastLine
+
--- FSharpSandbox/MiscScripts/Strata.fsx
+++ FSharpSandbox/MiscScripts/Strata.fsx
@@ -14,8 +14,11 @@ open FSharp.Charting
 // Helper functions
 let c x = Complex(x, 0.0)
 let i = Complex.ImaginaryOne
+let exp = MathNet.Numerics.Complex.exp
+module List =
+  let mapio mapping list = List.map (fun x -> (x, mapping x)) list
 
-// == Example from Strata paper, Kottke. == 
+// == Example from STRATA paper by Kottke. == 
 // Layers from the top to the bottom
 let rhoList = [1930.0; 2240.0] // [kg/m3]
 let vsList = [350.0; 1500.0] // [m/s]
@@ -24,10 +27,10 @@ let hList = [50.0] // [m] Last layer is of infinte thickness (and not in the lis
 
 // == Example 7.3 from Kramer's book. == 
 // Layers from the top to the bottom
-let rhoList = [125.0; 160.0] // [kg/m3]
-let vsList = [1500.0; 5000.0] // [m/s]
-let DList = [0.05; 0.02] // [-]
-let hList = [540.0] // [m] Last layer is of infinte thickness (and not in the list).
+//let rhoList = [125.0; 160.0] // [kg/m3]
+//let vsList = [1500.0; 5000.0] // [m/s]
+//let DList = [0.05; 0.02] // [-]
+//let hList = [540.0] // [m] Last layer is of infinte thickness (and not in the list).
 
 // Notes on 
 // N = kg*m/s2
@@ -37,39 +40,40 @@ let hList = [540.0] // [m] Last layer is of infinte thickness (and not in the li
 
 // Shear modulus. Expression taken from https://en.wikipedia.org/wiki/S-wave where it is stated beta^2 = mu / rho.
 let getG rho vs = rho * vs**2.0 
-// Equation (2.4a)
+// Complex shear modulus. Equation (2.4a)
+// D is the damping ratio. It ranges between zero (no damping) to one (complete damping).
 let getGStar G D = c G * (c 1.0 - c 2.0 * c D * c D + c 2.0 * i * c D * MathNet.Numerics.Complex.sqrt(c 1.0 - c D * c D))
-// Equation (2.4b)
+// Complex shear modulus in approximated form. Equation (2.4b)
+// This approximation is correct for D < 0.2.
 let getGStar_approx G D = c G * (c 1.0 + c 2.0 * i * c D)
-// Equation (2.3)
+// Shear wave velocity. Equation (2.3)
 let getVsStar GStar rho = MathNet.Numerics.Complex.sqrt(GStar / c rho)
 let getVsStar_v2 vs D = c vs * (c 1.0 + i * c D)
 
+// Lists of G, complex G and complex wave velocity
 let GList = List.map2 getG rhoList vsList
 let GStarList = List.map2 getGStar GList DList
 let vsStarList = List.map2 getVsStar GStarList rhoList
 let vsStarList_v2 = List.map2 getVsStar_v2 vsList DList
 
+// Returns the value of the transfer function, i.e. the ratio between
+// the amplitude on the top of the j-th layer and the top of the bedrock (n-th layer)
 // Index j starts at zero. (in the paper it is denoted as index m and it starts from 1)
-let TF j n omega =
-    // Equation (2.2)
+// Omega is the angular frequency. Omaga = 2 * pi / T = 2 * pi * f
+let TF j omega =
+    let n = GStarList.Length - 1 // Index of the bedrock layer.
+    // Complex wave number. Equation (2.2)
     let getKStar vsStar = c omega / vsStar
     let kStarList = List.map getKStar vsStarList
-    //let kStarList = List.map2 (fun vs D -> c omega / c vs * (c 1.0 - i * c D)) vsList DList
-    let a_v1 j = c rhoList.[j] * vsStarList.[j] / (c rhoList.[j+1] * vsStarList.[j+1]) // aStar
-    let a_v2 j = kStarList.[j] * GStarList.[j] / (kStarList.[j+1] * GStarList.[j+1] )
-    let a j = a_v2 j
-    let exp = MathNet.Numerics.Complex.exp
+    // Complex impedance ratio between j-th layer and j+1-th layer. Equation (2.6)
+    let a j = c rhoList.[j] * vsStarList.[j] / (c rhoList.[j+1] * vsStarList.[j+1])
+    // Recursive formulas for incomming and outgoing wave amplitudes. Developed by Kramer.
     let rec AB j =
         match j with
         | 0 -> (c 1.0, c 1.0)
         | j ->
             let (A, B) = AB (j - 1)
-// This is taken from Kottke but is probably incorrect.
-//          let newA = c 0.5 * A * (c 1.0 + a (j-1)) * exp(i * kStarList.[j-1] * c hList.[j-1] / c 2.0)
-//                   + c 0.5 * B * (c 1.0 - a (j-1)) * exp(-i * kStarList.[j-1] * c hList.[j-1] / c 2.0)
-//          let newB = c 0.5 * A * (c 1.0 - a (j-1)) * exp(i * kStarList.[j-1] * c hList.[j-1] / c 2.0)
-//                   + c 0.5 * B * (c 1.0 + a (j-1)) * exp(-i * kStarList.[j-1] * c hList.[j-1] / c 2.0)
+            // This is taken from Kramer's book. The version in Kottke's STRATA manual seems to be incorrect.
             let newA = c 0.5 * A * (c 1.0 + a (j-1)) * exp(i * kStarList.[j-1] * c hList.[j-1])
                      + c 0.5 * B * (c 1.0 - a (j-1)) * exp(-i * kStarList.[j-1] * c hList.[j-1])
             let newB = c 0.5 * A * (c 1.0 - a (j-1)) * exp(i * kStarList.[j-1] * c hList.[j-1])
@@ -77,12 +81,17 @@ let TF j n omega =
             newA, newB
     let (Aj, Bj) = AB j
     let (An, Bn) = AB n
-    //(Aj + Bj) / (An + Bn) // Surface-Within transfer function
-    (An + Bn) / (c 2.0 * An) * (Aj + Bj) / (An + Bn) // Surface-Outcrop transfer function
-                                            
+    // SurfaceOfJthLayer to Within transfer function, Eq (2.7).
+    (Aj + Bj) / (An + Bn)
+    // SurfaceOfJthLayer to Outcrop transfer function, Eq (2.9).
+    //(An + Bn) / (c 2.0 * An) * (Aj + Bj) / (An + Bn)
+
+// Amplification function.
+// Parameter f is the termporal frequency.                                          
 let tf f =
-    let omega = 2.0 * System.Math.PI * f
-    f, (TF 0 1 omega).Magnitude
+  // Angular frequency
+  let omega = 2.0 * System.Math.PI * f
+  f, (TF 0 omega).Magnitude
 
 [0.0..0.01..25.0] |> List.map tf |> FSharp.Charting.Chart.FastLine
 [0.1..0.01..500.0] |> List.map tf |> List.maxBy snd
@@ -95,24 +104,73 @@ let omega0 = System.Math.PI * vsList.Head / 2.0 / hList.Head
 // Play around with FFT. //
 // --------------------- //
 module Fourier = 
-    let options = MathNet.Numerics.IntegralTransforms.FourierOptions.Default
+  let options = MathNet.Numerics.IntegralTransforms.FourierOptions.Matlab
+  
+  // Forward FFT 
+  let fftf (list : Complex list) =
+    let res = (list |> List.toArray).Clone() :?> (Complex [])
+    MathNet.Numerics.IntegralTransforms.Fourier.Forward(res, options) 
+    res |> Array.toList
 
-    let f t = sin t
-    let ts = [0.0..0.01..10.0]
-    let fs = ts |> List.map f
+  // Inverse FFT 
+  let ffti (list : Complex list) =
+    let res = (list |> List.toArray).Clone() :?> (Complex [])
+    MathNet.Numerics.IntegralTransforms.Fourier.Inverse(res, options) 
+    res |> Array.toList
+  
+  // Function
+  let f t = cos t
+  let steps = 2.0**4.0
+  let T = 2.0 * System.Math.PI
+  let ts = [0.0 .. 1.0 / steps .. 1.0 - 1.0 / steps] |> List.map (fun fraction -> fraction * T)
+  let fs = ts |> List.map f
 
-    let array = fs |> List.map c |> List.toArray
-    MathNet.Numerics.IntegralTransforms.Fourier.Forward(array, options)
-    let invArray = array.Clone() :?> (Complex [])
-    MathNet.Numerics.IntegralTransforms.Fourier.Inverse(invArray, options) 
-    fs |> FSharp.Charting.Chart.Line
-    array |> Seq.map (fun x -> x.Magnitude) |> FSharp.Charting.Chart.Line
-    array |> Seq.map (fun x -> x.Phase) |> FSharp.Charting.Chart.Line
-    array |> Seq.map (fun x -> x.Real) |> FSharp.Charting.Chart.Line
-    array |> Seq.map (fun x -> x.Imaginary) |> FSharp.Charting.Chart.Line
-    invArray |> Seq.map (fun x -> x.Real) |> FSharp.Charting.Chart.Line
-    invArray |> Seq.map (fun x -> x.Imaginary) |> FSharp.Charting.Chart.Line // Should be zero. Small values are related to rounding error. 
+  // Forward DFT
+  let spectrum = fs |> List.map c |> fftf
+  // Inverse DFT
+  let signal = spectrum |> ffti
 
+  // Scaling coefficient for amplitudes in spectrum for "FourierOptions.Matlab".
+  let scalingCoeff = 2.0 / (float fs.Length)
+
+  // The spectrum must be multiplied by 2.0 because the rhs mirror image is missing.
+  let artifitialSpectrum = [c 0.0; c (2.0 * 1.0 / scalingCoeff)] @ List.replicate (int steps - 2) (c 0.0)
+  let artifitialSignal = artifitialSpectrum |> ffti
+
+  //----------//
+  // Charting //
+  //----------//
+
+  // 1. Input signal
+  fs |> FSharp.Charting.Chart.Line
+  
+  // 2. Spectrum
+  spectrum |> List.map (fun x -> x.Magnitude) |> FSharp.Charting.Chart.Line
+  spectrum |> List.map (fun x -> x.Phase) |> FSharp.Charting.Chart.Line
+  spectrum |> List.map (fun x -> x.Real) |> FSharp.Charting.Chart.Line
+  spectrum |> List.map (fun x -> x.Imaginary) |> FSharp.Charting.Chart.Line
+  // This should hold
+  spectrum.[1].Real * scalingCoeff = 1.0
+  
+  
+  // 3. Input signal generated from spectrum
+  signal |> List.map (fun x -> x.Real) |> FSharp.Charting.Chart.Line
+  signal |> List.map (fun x -> x.Imaginary) |> FSharp.Charting.Chart.Line // Should be zero. Small values are related to rounding error. 
+
+  // 4. Artificial spectrum and resulting signal
+  artifitialSpectrum |> Seq.map (fun x -> x.Real) |> FSharp.Charting.Chart.Line
+  artifitialSignal   |> Seq.map (fun x -> x.Real) |> FSharp.Charting.Chart.Line
+
+  let drawBoth (mapping : Complex -> float) =
+    let ch1 = signal           |> List.map mapping |> FSharp.Charting.Chart.Line
+    let ch2 = artifitialSignal |> List.map mapping |> FSharp.Charting.Chart.Line
+    [ch1; ch2] |> Chart.Combine
+
+  drawBoth (fun x -> x.Imaginary)
+
+
+// Play around with real data //
+// -------------------------- //
 
 // Get displacement data from file obtained from http://ngawest2.berkeley.edu/
 let fileName = @"c:\Users\Tomas\Sync\Fine\Earthquake\RSN305_ITALY.P_C-CNV000.DT2"
@@ -132,13 +190,64 @@ let timeIntervalLength = timeStep * (float numPoints)
 values |> FSharp.Charting.Chart.FastLine
 
 // FFT
-let options = MathNet.Numerics.IntegralTransforms.FourierOptions.Default
-let array = values |> Seq.map c |> Seq.toArray
-MathNet.Numerics.IntegralTransforms.Fourier.Forward(array, options)
-let result = array |> Seq.map (fun x -> x.Magnitude) |> Seq.toList |> List.take 100
-result |> FSharp.Charting.Chart.FastLine
+let signalOrig = values |> Seq.toList |> List.map c
+let spectrum = signalOrig |> Fourier.fftf
+spectrum |> List.map (fun x -> x.Magnitude) |> List.take 100 |> Chart.FastLine
+
+// Get truncated spectrum
+let numTrunc = 100
+let spectrumTrunc = (spectrum |> List.take numTrunc |> List.map (fun x -> c 2.0 * x)) @ List.init (numPoints - numTrunc) (fun _ -> c 0.0)
+
+let signal = spectrum |> Fourier.ffti 
+let signalTrunc = spectrumTrunc |> Fourier.ffti
+
+let mapping (data : Complex list) = data |> List.map (fun x -> x.Real) |> Chart.FastLine
+[signalOrig; signal; signalTrunc] |> List.map mapping |> Chart.Combine
+
+module Spectrum =
+  // For given parameters returns spectrum as a function of time period T [s].
+  let getSpectrum (tb, tc, td, eta, ag, s) =
+    let se t =
+      match t with
+      | t when t < 0.0 -> failwith "Negative period time T."
+      | t when t < tb -> ag * s * (1.0 + t / tb * (2.5 * eta - 1.0))
+      | t when t < tc -> 2.5 * eta * ag * s
+      | t when t < td -> 2.5 * eta * ag * s * tc / t
+      | t when t < 4.0 -> 2.5 * eta * ag * s * tc * td / t**2.0
+      | _ -> 0.0 // For periods longer than 4 seconds.
+    se
+
+let mySpectrum = Spectrum.getSpectrum (0.1, 0.5, 2.0, 0.5, 1.0, 1.0)
+let mySpectrumInF f = mySpectrum (1.0 / f)
+
+// Required parameters for accelerogram
+let dt = 0.01
+let tTot = 10.0
+let n = tTot / dt + 1.0 |> int
+[0.0 .. dt .. tTot] |> List.mapio mySpectrum |> Chart.FastLine
+
+// Sampling frequency
+let fs = 1.0 / dt
+// frequency step
+let df = fs / float (n - 1)
+
+let frequencies = [0.0 .. df .. fs]
+let spectrum = frequencies |> List.map mySpectrumInF |> List.map c
+let take = 200
+let spectrum' = (spectrum |> List.take take) @ List.replicate (n - take) (c 0.0)
+List.map2 (fun f (x : Complex) -> f, x.Real) frequencies spectrum' |> Chart.FastPoint
+
+let times = List.init spectrum.Length (fun i -> float i * dt)
+let signal = spectrum' |> Fourier.ffti
+List.map2 (fun t (x : Complex) -> t, x.Real) times signal |> List.take 100 |> Chart.FastLine
 
 
+List.replicate 10 (c 1.0) |> Fourier.ffti |> List.map (fun x -> x.Real) |> Chart.Line
+
+
+
+
+// Draw exponent function with complex parameter
 module ComplExp =
   let c x = System.Numerics.Complex(x, 0.0)
   let f a x = MathNet.Numerics.Complex.exp(a * c x)
